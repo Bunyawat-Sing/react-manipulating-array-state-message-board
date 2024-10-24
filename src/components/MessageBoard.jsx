b4b165b91627cc951a35acb697b9c1fb67f2ee0b
Bunyawat Singkiporn <pupahero@gmail.com>
2024-10-24 13:55:46 +0700

    feat: adding and removing array state

--- src/components/MessageBoard.jsx
+++ src/components/MessageBoard.jsx
@@ -1,4 +1,20 @@
+import { useState } from "react";
+
 function MessageBoard() {
+  const [message, setMessage] = useState([]);
+  const [inputMessage, setInputMessage] = useState("");
+
+  const addMessage = (event) => {
+    event.preventDefault();
+    const newMessage = [...message, inputMessage];
+    setMessage(newMessage);
+  };
+
+  const deleteMessage = (indexMessage) => {
+    const newMessage = message.filter((_, index) => index !== indexMessage);
+    setMessage(newMessage);
+  }
+
   return (
     <div className="app-wrapper">
       <h1 class="app-title">Message board</h1>
@@ -9,15 +25,29 @@ function MessageBoard() {
             name="message-text"
             type="text"
             placeholder="Enter message here"
+            value={inputMessage}
+            onChange={(event) => setInputMessage(event.target.value)}
           />
         </label>
-        <button className="submit-message-button">Submit</button>
+        <button onClick={addMessage} className="submit-message-button">
+          Submit
+        </button>
       </div>
-      <div class="board">
-        <div className="message">
+      <div className="board">
+        {
+          message.map((input, index) => {
+            return (
+              <div key={index} className="message">
+                <h1>{input}</h1>
+                <button onClick={()=>{deleteMessage(index)}} className="delete-button">x</button>
+              </div>
+            );
+          })
+          /*<div className="message">
           <h1>Hello all ! This is first message.</h1>
           <button className="delete-button">x</button>
-        </div>
+        </div>*/
+        }
       </div>
     </div>
   );
