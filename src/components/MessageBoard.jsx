a67ed3b1fff8430e3e45669080d629f36efc66b5
Arkorn Pattanavibul <arkorn.pat@gmail.com>
2024-10-24 13:42:15 +0700

    feat:add useState for add input&delete messages

--- src/components/MessageBoard.jsx
+++ src/components/MessageBoard.jsx
@@ -1,23 +1,51 @@
+import { useState } from "react";
+
 function MessageBoard() {
+  const [messages, setMessages] = useState([]); 
+  const [inputMessage, setInputMessage] = useState("");
+
+
+  function addMsg() {
+    const newMsg = {
+      id: messages.length + 1,
+      message: inputMessage
+    };
+
+    setMessages([...messages, newMsg]);
+    setInputMessage(""); 
+    // const newarr = [...message, inputmessage]
+    // setMessage(newarr)
+    // setInputMessage("")
+  }
+
+  function deleteMsg(id) { 
+    const newArr = messages.filter((msg) => msg.id !== id); 
+    setMessages(newArr);
+  }
+
   return (
     <div className="app-wrapper">
-      <h1 class="app-title">Message board</h1>
-      <div class="message-input-container">
+      <h1 className="app-title">Message board</h1>
+      <div className="message-input-container">
         <label>
           <input
             id="message-text"
             name="message-text"
             type="text"
             placeholder="Enter message here"
+            value={inputMessage}
+            onChange={(e)=> setInputMessage(e.target.value)}
           />
         </label>
-        <button className="submit-message-button">Submit</button>
+        <button className="submit-message-button" onClick={addMsg}>Submit</button>
       </div>
-      <div class="board">
-        <div className="message">
-          <h1>Hello all ! This is first message.</h1>
-          <button className="delete-button">x</button>
-        </div>
+      <div className="board">
+        {messages.map((text) => (
+          <div className="message" key={text.id}>
+            <h1>{text.message}</h1>
+            <button className="delete-button" onClick={()=>deleteMsg(text.id)}>x</button>
+          </div>
+        ))}
       </div>
     </div>
   );
